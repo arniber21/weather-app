796a8d7d1758f0415ae078f7543b5fafa48cdf36
Arnab Ghosh <arnabcare21@gmail.com>
2021-07-26 19:47:55 -0600

    added very basic weather functionality

---
+++ app.js
@@ -0,0 +1,18 @@
+let userLocation = document.querySelector("#userLocation");
+let submit = document.querySelector("#button");
+let statusIndicator = document.querySelector("#weatherStatus")
+
+let APIURL = "http://api.weatherapi.com/v1/current.json";
+let apiKey = "f8500184ef0a4655be611608212707";
+
+submit.addEventListener("click", function() {
+    fetch(`${APIURL}?key=${apiKey}&q=${userLocation.value}&aqi=no`)
+    .then(res => res.json())
+    .then((res)=> {
+        console.log(res)
+        statusIndicator.innerHTML = `${res.current.condition.text} and ${Math.round(res.current.temp_f)} degrees in ${res.location.name}, ${res.location.region}`
+    }).catch(e => {
+        statusIndicator.innerHTML = `Something went wrong! Re-enter location or test your internet`
+        console.log(e)
+    })
+});
